71083126981583a30355d459fb86f8fe68bc5cee
Stanislav D. Kudriavtsev <staniskudriavtsev@yandex.ru>
2022-06-05 13:49:00 +0300

    a prototype of the Prototype

---
+++ prototype/python/prototype.py
@@ -0,0 +1,20 @@
+# Inspired by "Practical Python Design Patterns", Wessel Badenhorst
+# https://radek.io/2011/08/03/design-pattern-prototype/
+
+
+from abc import ABC, abstractmethod
+from copy import deepcopy
+
+
+class Cloner(ABC):
+	"""Prototype creational design pattern."""
+
+	@abstractmethod
+	def clone(self):
+		raise NotImplemented
+
+class VirusCloner(Cloner):
+	"""A concrete cloner associated with a (Virus) client."""
+
+	def clone(self):
+		return deepcopy(self)
