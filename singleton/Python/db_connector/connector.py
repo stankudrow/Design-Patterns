9b015a08e310a80a9675cd7e79d3a81fdd6cc381
Stanley Kudrow <63244186+stankudrow@users.noreply.github.com>
2023-01-05 02:20:10 +0300

    singleton: db_connector example

---
+++ singleton/Python/db_connector/connector.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# 1. cd here
+# 2. run `python3 logger.py`
+
+import sqlite3
+from sys import path
+
+path.insert(0, "..")
+
+from singleton import Singleton
+
+@Singleton
+class SQLDataBase:
+    """SQLite database [file] class."""
+
+    def __init__(self, conn: str):
+        self.conn = sqlite3.connect(conn)
+        self.cur = self.conn.cursor()
+
+    def execute(self, stmt: str):
+        return self.cur.execute(stmt)
+
+
+if __name__ == "__main__":
+    dbc1 = SQLDataBase("tutorial.db")
+    dbc2 = SQLDataBase("tutorial.db")
+    assert id(dbc1) == id(dbc2)
+    dbc1.execute("CREATE TABLE IF NOT EXISTS movie(title, year, score)")
+    res = dbc2.execute("SELECT name FROM sqlite_master")
+    print(f"DB name -> {res.fetchone()}")
